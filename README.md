70d87d27fe6f131acc2762a718eef378703558f7
Rayhan Armand <80816570+rrayhann@users.noreply.github.com>
2024-01-19 22:25:18 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Task 5 Project by Rayhan Achmad Armand
+
+## Installation
+
+To run this project, please follow these steps:
+run npm install to install dependencies
+
+## usage
+
+heres how you can use this project
+
+1. make sure to install node.js / check node js on terminal : node -v
+
+2. install prompt package : npm install prompt-sync
+
+3. add this line
+
+   const prompt = require("prompt-sync")({ sigint: true });
+
+4. make sure on terminal, the directory is set to this folder, to check, on terminal
+   type: pwd
+
+5. if yes continue, if not, write: cd path/to/this/folder
+
+6. open integrated terminal and type: node task5.js
+
+7. enter number :)
---
+++ task5.js
@@ -0,0 +1,30 @@
+function calculateA(a, b, c, d) {
+  //check if each value is REAL number
+  if (
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    typeof c !== "number" ||
+    typeof d !== "number"
+  ) {
+    throw new Error("Input Error, Please enter REAL number!🥰");
+  }
+  //check if c plus d is not equal to zero
+  if (c + d === 0) {
+    throw new Error(
+      "zero is not allowed for c and d, Please enter REAL number for c and d!"
+    );
+  }
+  //run calculation if
+  return (a + b) / (c + d);
+}
+
+try {
+  const a = parseFloat(prompt("Please enter value of A:"));
+  const b = parseFloat(prompt("Please enter value of B:"));
+  const c = parseFloat(prompt("Please enter value of C:"));
+  const d = parseFloat(prompt("Please enter value of D:"));
+  const result = calculateA(a, b, c, d);
+  console.log("Result:", result);
+} catch (error) {
+  console.error(error.message);
+}
